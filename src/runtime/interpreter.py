5198c19ca47df76405a2ac1642ded54ca7434eac
Vishvam10 <s.vishvam1025@gmail.com>
2024-03-19 23:00:46 +0530

    refactor: move all interpret functions into a class

--- src/runtime/interpreter.py
+++ src/runtime/interpreter.py
@@ -1,102 +1,112 @@
 from typing import cast
 
-from .values import ValueType, RuntimeVal, NumberVal, make_null, make_number
+from .values import RuntimeVal, NumberVal, make_null, make_number
 from frontend.ast import NodeType, Stmt, Identifier, VariableDecl, AssignmentExpr, BinaryExpr, Program
 from runtime.environment import Environment
 
+class Interpreter() :
 
-def _evaluate_numeric_binary_expr(left: NumberVal, right: NumberVal, operator: str) -> NumberVal:
+    def __init__(self, env : Environment) -> None:
+        self.env = env
+        return
 
-    res = 0
+    def _evaluate_numeric_binary_expr(self, left: NumberVal, right: NumberVal, operator: str) -> NumberVal:
 
-    if (operator == '+'):
-        res = left.value + right.value
-    elif (operator == '-'):
-        res = left.value - right.value
-    elif (operator == '*'):
-        res = left.value * right.value
-    elif (operator == '/'):
-        if (right.value != 0):
-            res = left.value / right.value
+        res = 0
+
+        if (operator == '+'):
+            res = left.value + right.value
+        elif (operator == '-'):
+            res = left.value - right.value
+        elif (operator == '*'):
+            res = left.value * right.value
+        elif (operator == '/'):
+            if (right.value != 0):
+                res = left.value / right.value
+            else:
+                print('\n[INTERPRETER ERROR] : Division by 0')
+                exit(0)
+        elif (operator == '%'):
+            res = left.value % right.value
+
+        return make_number(res)
+
+    def _evaluate_binary_expr(self, expr: BinaryExpr) -> RuntimeVal:
+
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        if (isinstance(left, NumberVal) and isinstance(right, NumberVal)):
+            return self._evaluate_numeric_binary_expr(left, right, expr.operator)
+
+        return make_null()
+
+    def _evaluate_identifier(self, ident: Identifier) -> RuntimeVal:
+
+        val = self.env.lookup_var(ident.symbol)
+        return val
+
+    def _evaluate_variable_decl(self, decl: VariableDecl) -> RuntimeVal:
+
+        val = make_null()
+
+        if (decl.value):
+            val = self.evaluate(decl.value)
+        
+        return self.env.decl_var(var_name=decl.identifier, value=val, constant=decl.constant)
+
+    def _evaluate_assignment(self, node : AssignmentExpr) :
+
+        print('kind : ', node.assignee.kind)
+        if(node.assignee.kind != NodeType.Identifier) :
+            print(f'\n[INTERPRETER ERROR] : Invalid LHS inside assignmenr expr : {node.assignee.to_dict()}')
+            exit(0)
+        
+        var_name = cast(Identifier, node.assignee).symbol
+        value = self.evaluate(node.value)
+       
+        return self.env.assign_var(var_name, value)
+
+
+
+    def _evaluate_program(self, program: Program) -> RuntimeVal:
+
+        last_evaluated: RuntimeVal = make_null()
+
+        for stmt in program.body:
+            last_evaluated = self.evaluate(stmt)
+
+        return last_evaluated
+
+
+    def evaluate(self, ast_node: Stmt) -> RuntimeVal:
+
+        print(ast_node, type(ast_node))
+
+        if (ast_node == None):
+            print('\n[INTERPRETER ERROR] : AST node is None : ', ast_node)
+            exit(0)
+
+        if (ast_node.kind == NodeType.NumericalLiteral):
+            return NumberVal(value=ast_node.value)
+        
+        elif(ast_node.kind == NodeType.AssignmentExpr) :
+            return self._evaluate_assignment(ast_node)
+
+        elif (ast_node.kind == NodeType.BinaryExpr):
+            return self._evaluate_binary_expr(ast_node)
+
+        elif (ast_node.kind == NodeType.Identifier):
+            return self._evaluate_identifier(ast_node)
+
+        elif (ast_node.kind == NodeType.VariableDecl):
+            return self._evaluate_variable_decl(ast_node)
+
+        elif (ast_node.kind == NodeType.Program):
+            return self._evaluate_program(ast_node)
+
         else:
-            print('[INTERPRETER ERROR] : Division by 0')
-    elif (operator == '%'):
-        res = left.value % right.value
+            print('\n[INTERPRETER ERROR] :  This AST node has not been yet been setup for interpretation : ', ast_node.to_dict())
+            exit(0)
 
-    return make_number(res)
-
-
-def _evaluate_binary_expr(expr: BinaryExpr, env: Environment) -> RuntimeVal:
-
-    left = evaluate(expr.left, env)
-    right = evaluate(expr.right, env)
-
-    if (isinstance(left, NumberVal) and isinstance(right, NumberVal)):
-        return _evaluate_numeric_binary_expr(left, right, expr.operator)
-
-    return make_null()
-
-
-def _evaluate_identifier(ident: Identifier, env: Environment) -> RuntimeVal:
-
-    val = env.lookup_var(ident.symbol)
-    return val
-
-
-def _evaluate_variable_decl(decl: VariableDecl, env: Environment) -> RuntimeVal:
-
-    val = make_null()
-
-    if (decl.value):
-        val = evaluate(decl.value, env)
-
-    return env.decl_var(var_name=decl.identifier, value=val, constant=decl.constant)
-
-def _evaluate_assignment(node : AssignmentExpr, env : Environment) :
-
-    if(not isinstance(node.assignee.kind, Identifier)) :
-        print(f'[INTERPRETER ERROR] : Invalid LHS inside assignmenr expr : {node.assignee.to_dict()}')
-    
-    var_name = cast(Identifier, node.assignee).symbol
-    value = evaluate(node.value, env)
-    env.assign_var(var_name, value)
-
-def _evaluate_program(program: Program, env: Environment) -> RuntimeVal:
-
-    last_evaluated: RuntimeVal = make_null()
-
-    for stmt in program.body:
-        last_evaluated = evaluate(stmt, env)
-
-    return last_evaluated
-
-
-def evaluate(ast_node: Stmt, env: Environment) -> RuntimeVal:
-
-    print('ast node : ', ast_node)
-
-    if (ast_node == None):
-        print('[INTERPRETER ERROR] : AST node is None : ', ast_node)
-        exit(0)
-
-    if (ast_node.kind == NodeType.NumericalLiteral):
-        return NumberVal(value=ast_node.value)
-    
-    elif(ast_node.kind == NodeType.AssignmentExpr) :
-        return _evaluate_assignment(ast_node, env)
-
-    elif (ast_node.kind == NodeType.BinaryExpr):
-        return _evaluate_binary_expr(ast_node, env)
-
-    elif (ast_node.kind == NodeType.Identifier):
-        return _evaluate_identifier(ast_node, env)
-
-    elif (ast_node.kind == NodeType.VariableDecl):
-        return _evaluate_variable_decl(ast_node, env)
-
-    elif (ast_node.kind == NodeType.Program):
-        return _evaluate_program(ast_node, env)
-
-    else:
-        print('[INTERPRETER ERROR] :  This AST node has not been yet been setup for interpretation : ', ast_node.to_dict())
-        exit(0)
+        
